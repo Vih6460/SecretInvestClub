1ee8f5d08ef21abfc5474cad15e3410655804e54
Vinicius <viniciuz6460@gmail.com>
2021-12-16 12:05:01 -0300

    tirando redirecionamento da logo no dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -65,7 +65,7 @@ if(!isset($_SESSION['conta'])){
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a class="brand-link">
       <img src="../src/img/$comEscudo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8; border-radius: 0!important; box-shadow: none!important;">
       <span class="brand-text font-weight-light">Secret Invest Club</span>
     </a>
